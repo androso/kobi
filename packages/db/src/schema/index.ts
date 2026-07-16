ffbf0e23bde09babc849ab24f8d8b01c72214f2c
Isaac <dev.zacknmr28@gmail.com>
2026-07-15 21:06:16 -0600

    feat(db): export curriculum selection schema and update README

--- packages/db/src/schema/index.ts
+++ packages/db/src/schema/index.ts
@@ -9,6 +9,7 @@ export * from "./segments.js";
 export * from "./checkpoints.js";
 export * from "./curriculumChunks.js";
 export * from "./curriculumSources.js";
+export * from "./curriculumSourceSelections.js";
 export * from "./activityBundles.js";
 export * from "./activities.js";
 export * from "./sessionActivityCandidates.js";
